f63a56e7410c4a6c71cd79063023c57b4f62e177
Brian Ross <brian.ross@students.makeschool.com>
2020-09-17 22:45:23 -0400

    displayed mood pics and add to cart

---
+++ resources/js/scripts.js
@@ -0,0 +1,33 @@
+import data from './data.js'
+const itemsContainer = document.getElementById('items')
+
+for (let i=0; i<data.length; i++) {
+    let newDiv = document.createElement('div');
+    newDiv.className = 'item'
+
+
+
+    let img = document.createElement('img')
+    let desc = document.createElement('P')
+    desc.innerText = data[i].desc
+    newDiv.appendChild(desc)
+    img.src = data[i].image
+    img.width = 300
+    img.height = 300
+
+    newDiv.appendChild(img)
+    itemsContainer.appendChild(newDiv)
+
+    let price = document.createElement('P')
+    price.innerText = data[i].price
+    newDiv.appendChild(price)
+
+    let button = document.createElement('button')
+    button.id = data[i].name
+
+    button.dataset.price = data[i].price
+    button.innerHTML = 'Add to Cart'
+    newDiv.appendChild(button)
+    itemsContainer.appendChild(newDiv)
+    
+}
