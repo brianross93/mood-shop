6f7300de2b6e9b211d7e22d5cfbf4436ad322f54
Brian Ross <brian.ross@students.makeschool.com>
2020-09-26 23:42:50 -0400

    Console log price and qty working

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -38,7 +38,8 @@ let cart = [ ];
 function addItem(name, price) {
     for (let i=0;i<cart.length; i ++) {
         if (cart[i].name === name) {
-            
+            cart[i].qty += 1
+            return
         }
     }
     const item = {
@@ -50,13 +51,36 @@ function addItem(name, price) {
 }
 
 function showItems() {
-    console.log(`You have ${cart.length} items in your
-    cart`)
+    console.log(`You have ${getQty()} items in your cart`)
+    console.log(`Total price is: ${addPrice()} `)
     for (let i=0; i < cart.length; i++) {
         console.log(` ${cart[i].name} $${cart[i].price} x ${cart[i].qty}`)
+
     }
 }
+function addPrice() {
+    
+    let total_Price = 0
+    for (let i=0; i < cart.length; i++) {
+        total_Price += (cart[i].price * cart[i].qty)
+    }   return total_Price.toFixed(2);
+    
+}
+
+function getQty() {
+    let qty = 0
+    for (let i=0; i< cart.length; i++) {
+        qty += cart[i].qty
+    }
+    return qty;
+}
+
+
+
 
 addItem('Apple', 0.99)
 addItem('Orange', 1.29)
+addItem('Apple', 0.99)
+addItem('Opinion', 0.02)
+addItem('Frisbee', 9.62)
 showItems();
